a3d8feef6dc563ce39e42349f3990afc5bc24995
25LP0902 25LP0902 <25LP0902@admins-iMac-2.local>
2025-12-09 10:36:21 +0800

    added foodCound json

--- src/resolvers/food-category/getCategories.js
+++ src/resolvers/food-category/getCategories.js
@@ -2,12 +2,32 @@ import { foodCategoryModel } from "../../model/foodCategoryModel.js";
 
 export const getCategories = async (req, res) => {
   try {
-    const categories = await foodCategoryModel.find();
+    const categories = await foodCategoryModel.aggregate([
+      {
+        $lookup: {
+          from: "foods",
+          localField: "_id",
+          foreignField: "category",
+          as: "foods",
+        },
+      },
+      {
+        $addFields: {
+          foodCount: { $size: "$foods" },
+        },
+      },
+      {
+        $project: {
+          foods: 0,
+        },
+      },
+    ]);
+
     res.status(200).json(categories);
-  } catch (err) {
+  } catch (error) {
     console.error("Error fetching food categories:", error);
-    res
-      .status(500)
-      .json({ message: "Server error while fetching food categories." });
+    res.status(500).json({
+      message: "Server error while fetching food categories.",
+    });
   }
 };
